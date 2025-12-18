3ce063bdd366a4225cc02f0c285786dd0af19186
Đặng Đức Minh <161326799+DMinh1102@users.noreply.github.com>
2025-12-19 05:29:53 +0700

    Fixes missing images
    I'm done:(

---
+++ smartshop-backend/src/main/resources/db/migration/V17__fix_product_image_urls.sql
@@ -0,0 +1,29 @@
+-- Fix incorrect product image URLs from V16 migration
+-- Replace broken/external URLs with reliable Unsplash images
+
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1544244015-0df4b3ffc6b0?w=800' WHERE name = 'iPad Pro 12.9"';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1561154464-82e9adf32764?w=800' WHERE name = 'Samsung Galaxy Tab S9';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?w=800' WHERE name = 'Lenovo ThinkPad X1 Carbon Gen 11';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1603302576837-37561b2e2302?w=800' WHERE name = 'HP Pavilion Gaming';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1606841837239-c5a1a4a07af7?w=800' WHERE name = 'AirPods Pro (2nd Gen)';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1609091839311-d5365f9ff1c5?w=800' WHERE name = 'Anker PowerCore 20,000mAh';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1527814050087-3793815479db?w=800' WHERE name = 'Logitech MX Master 3S';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=800' WHERE name = 'Casio G-Shock GA-2100';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1524592094714-0f0654e20314?w=800' WHERE name = 'Citizen Eco-Drive';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800' WHERE name = 'Adidas Ultraboost 23';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?w=800' WHERE name = 'Vans Old Skool';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1596755094514-f87e34085b2c?w=800' WHERE name = 'Uniqlo Áo sơ mi Oxford';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1473966968600-fa801b869a1a?w=800' WHERE name = 'Quần kaki nam';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1595777457583-95e059d581b8?w=800' WHERE name = 'Đầm maxi nữ';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1556228578-8c89e6adf883?w=800' WHERE name = 'Innisfree Green Tea Foam Cleanser';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1620916566398-39f1143ab7be?w=800' WHERE name = 'CeraVe Moisturizing Cream';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1532413992378-f169ac26fff0?w=800' WHERE name = 'La Roche-Posay Sunscreen SPF50+';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1585659722983-3a675dabf23d?w=800' WHERE name = 'Lò vi sóng Sharp 20L';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1544025162-d76694265947?w=800' WHERE name = 'Philips Nồi cơm điện 1.8L';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1570222094114-d054a817e56b?w=800' WHERE name = 'Máy xay sinh tố Oster';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=800' WHERE name = 'Royal Canin Puppy Food';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?w=800' WHERE name = 'Kong Classic Toy';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1593095948071-474c5cc2989d?w=800' WHERE name = 'Optimum Nutrition Whey Protein';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1505751172876-fa1923c5c528?w=800' WHERE name = 'Nature Made Omega-3';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?w=800' WHERE name = 'Tạ tay cao su 5kg (đôi)';
+UPDATE products SET image_url = 'https://images.unsplash.com/photo-1598289431512-b97b0917affc?w=800' WHERE name = 'Dây kháng lực tập gym';
